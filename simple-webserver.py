11a3d0a86ac8e749e14ba498fa680a3d06642ce1
alex <alexcrafteo1@outlook.es>
2022-05-26 12:04:47 +0200

    WIP

--- simple-webserver.py
+++ simple-webserver.py
@@ -4,6 +4,10 @@ import argparse
 
 DEFAULT_PORT = 8080
 
+def logIp(address):
+    print("GET request: %s", address)
+
+
 class HttpHandler(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
@@ -14,23 +18,23 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes("</html>", "utf-8"))
 
     def do_GET(self):
-        print("GET request: %s", str(self.client_address))
+        logIp(str(self.client_address))
         self._set_response()
 
 def run(server_class=HTTPServer, handler_class=HttpHandler, port=DEFAULT_PORT):
     server_address = ('0.0.0.0', port)
     httpd = server_class(server_address, handler_class)
-    print('Servidor web iniciado en el puerto', port)
+    print('Web servers initialized in port', port)
     httpd.serve_forever()
 
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print('Deteniendo servidor web...\n')
+        print('Stopping web server...\n')
         httpd.server_close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("port", nargs='?', type=int, help="Listen port of the socket server. (Default " + str(DEFAULT_PORT) + ")", default=DEFAULT_PORT)
+    parser.add_argument("port", nargs='?', type=int, help="Listen port of the web server. (Default " + str(DEFAULT_PORT) + ")", default=DEFAULT_PORT)
 
     run(port=parser.parse_args().port)
